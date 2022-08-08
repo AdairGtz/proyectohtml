38c7b5a5f249c0c29b26226afdf81cc2fd4d02c3
AdairGtz <adairgt98@gmail.com>
2022-08-08 10:48:21 -0500

    Adds test to make sure no specific

--- sp_insertar.php
+++ sp_insertar.php
@@ -5,7 +5,7 @@
     $existencia= $_POST['existencia'];
 
     $cnx = mysqli_connect("sql200.epizy.com", "epiz_30541427", "iiYMLZlWwk49", "epiz_30541427_mercancia");
-    $sql= "INSERT INTO producto(codigo, nombre, existencia) VALUES('$codigo','$nombre','$existencia')";
+    $sql= "INSERT INTO producto(codigo, nombre, existencia) VALUES('01','Taza','ff')";
     $rta= mysqli_query($cnx, $sql);
     if (!$rta) {
         echo "No se inserto!";
