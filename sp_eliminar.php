ce9834cb23dd5c428de526a88b939dff7de0b16f
AdairGtz <adairgt98@gmail.com>
2022-08-08 10:26:00 -0500

    Adds test to make sure no specific

--- sp_eliminar.php
+++ sp_eliminar.php
@@ -2,7 +2,7 @@
 
     $id= $_GET['id'];
 
-    $cnx = mysqli_connect("sql200.epizy.com", "epiz_30541427", "iiYMLZlWwk49", "epiz_30541427_mercancia");
+    $cnx = mysqli_connect("ffff", "epiz_30541427", "iiYMLZlWwk49", "epiz_30541427_mercancia");
     $sql= "DELETE FROM producto where id like $id";
     $rta= mysqli_query($cnx, $sql);
     if (!$rta) {
